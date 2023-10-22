8f8216b071145f3c8c91234e1c1138c917fbcdc4
minseo300 <lmslmsms0616@gmail.com>
2023-10-22 15:55:24 +0900

    Feat: 섹션 6 완료

--- section4~/src/main/java/inflearn/advanced/ProxyApplication.java
+++ section4~/src/main/java/inflearn/advanced/ProxyApplication.java
@@ -6,6 +6,8 @@ import inflearn.advanced.config.v1_proxy.ConcreteProxyConfig;
 import inflearn.advanced.config.v1_proxy.InterfaceProxyConfig;
 import inflearn.advanced.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import inflearn.advanced.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import inflearn.advanced.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import inflearn.advanced.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import inflearn.advanced.trace.logtrace.LogTrace;
 import inflearn.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -25,7 +27,9 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 
 @SpringBootApplication(scanBasePackages = "inflearn.advanced.app")
 
